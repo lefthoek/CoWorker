c3295788142071c98d8548c27cfdac2b1b56f37c
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-08-09 12:50:30 +0200

    refactors

--- prototypes/researcher/src/mine.ts
+++ prototypes/researcher/src/mine.ts
@@ -1,8 +1,9 @@
 import { getAllChannelMessages } from "./adapters/slack";
-import data_lake from "./stores/data_lake";
+import Datalake from "./stores/data_lake";
 import { StatusCodes } from "./types";
 
 const mine = async ({ channel_id }: { channel_id: string }) => {
+  const data_lake = new Datalake({ channel_id });
   const timestamp = data_lake.getLatestTimestamp();
   const messageIterator = await getAllChannelMessages({
     channelId: channel_id,
--- prototypes/researcher/src/stores/data_lake.ts
+++ prototypes/researcher/src/stores/data_lake.ts
@@ -3,19 +3,25 @@ import { StatusCodes } from "../types";
 
 class DataLake {
   buffer: Record<string, any>[];
-  dirName: string;
+  root_dir_name: string;
+  dir_name: string;
 
-  constructor({ dirName }: { dirName: string }) {
-    this.dirName = dirName;
-    if (!fs.existsSync(this.dirName)) {
-      console.log(`new datalake: ${this.dirName}`);
-      fs.mkdirSync(this.dirName);
+  constructor({ channel_id }: { channel_id: string }) {
+    this.root_dir_name = "./datalake";
+    this.dir_name = `${this.root_dir_name}/${channel_id}/`;
+    if (!fs.existsSync(this.root_dir_name)) {
+      console.log(`new datalake: ${this.root_dir_name}`);
+      fs.mkdirSync(this.root_dir_name);
+    }
+    if (!fs.existsSync(this.dir_name)) {
+      console.log(`new repo: ${this.dir_name}`);
+      fs.mkdirSync(this.dir_name);
     }
     this.buffer = [];
   }
 
   getTimestamps() {
-    const timestamps = fs.readdirSync(this.dirName).map((file) => {
+    const timestamps = fs.readdirSync(this.dir_name).map((file) => {
       return file.replace(/.json$/, "");
     });
     return timestamps.sort().reverse();
@@ -36,7 +42,7 @@ class DataLake {
     if (!timestamp) {
       return [StatusCodes.INFO, "no new messages"];
     }
-    const path = `${this.dirName}/${timestamp}.json`;
+    const path = `${this.dir_name}/${timestamp}.json`;
     fs.writeFileSync(path, JSON.stringify(this.buffer));
     this.buffer = [];
     return [StatusCodes.SUCCESS, this.getLatestTimestamp()];
@@ -47,6 +53,4 @@ class DataLake {
   }
 }
 
-export default new DataLake({
-  dirName: "./data_lake",
-});
+export default DataLake;
