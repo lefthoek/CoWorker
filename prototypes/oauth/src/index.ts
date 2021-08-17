51ef48623675b8de0ef08d6b3126a0d206662821
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-08-17 22:13:15 +0200

    buckets

--- prototypes/oauth/src/index.ts
+++ prototypes/oauth/src/index.ts
@@ -1,39 +1,41 @@
 import fetch from "node-fetch";
+import AWS from "aws-sdk";
 
 module.exports.slack = async (event: any) => {
-  const clientId = process.env.SLACK_CLIENT_ID;
-  console.log(clientId);
-  const clientSecret = process.env.SLACK_CLIENT_SECRET;
+  const s3 = new AWS.S3();
+  const { SLACK_CLIENT_SECRET, SLACK_CLIENT_ID, DATALAKE_BUCKET } = process.env;
   const { code } = event.queryStringParameters;
+  if (!DATALAKE_BUCKET) {
+    throw "ERROR";
+  }
+  const Bucket = DATALAKE_BUCKET;
 
   const oauthURL =
     "https://slack.com/api/oauth.v2.access?" +
     "client_id=" +
-    clientId +
+    SLACK_CLIENT_ID +
     "&" +
     "client_secret=" +
-    clientSecret +
+    SLACK_CLIENT_SECRET +
     "&" +
     "code=" +
     code;
 
   const response = await fetch(oauthURL);
   const data = await response.json();
+  const id = "test";
 
-  console.log(data);
+  const Key = `${id}.json`;
+  const reply = await s3
+    .putObject({
+      Bucket,
+      Key,
+      Body: JSON.stringify(data, null, 2),
+    })
+    .promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: data,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(reply, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
