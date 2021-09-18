bafcc7041318a02389a62aeeb62fa8fe3d15021d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-09-18 22:53:11 +0200

    refactor

--- sites/superconnectors/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-import ContestantForm from './ContestantForm.svelte';
-import LeaderBoard from './LeaderBoard.svelte';
-import LefthoekLogo from './LefthoekLogo.svelte';
-
-export { ContestantForm, LeaderBoard, LefthoekLogo };
