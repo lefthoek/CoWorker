e52d8c672d8889f7a2b81f1159634ec0523f88c2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2021-09-23 12:33:54 +0200

    back to old scores

--- sites/superconnectors/src/routes/leaderboard.svelte
+++ sites/superconnectors/src/routes/leaderboard.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import LeaderBoard from '$components/LeaderBoard.svelte';
 	import leaderStore from '$stores/leaders';
-	import { time } from '$stores/time';
+	import { countdown } from '$stores/time';
 	const formatter = new Intl.DateTimeFormat('en', {
 		hour12: false,
-		hour: 'numeric',
 		minute: '2-digit',
 		second: '2-digit'
 	});
@@ -12,5 +11,5 @@
 
 <LeaderBoard fullscreen={true} leaderData={$leaderStore}>
 	<p class="text-background font-mono text-3xl" slot="header">LeaderBoard</p>
-	<p class="text-background font-mono text-3xl" slot="footer">{formatter.format($time)}</p>
+	<p class="text-background font-mono text-3xl" slot="footer">{formatter.format($countdown)}</p>
 </LeaderBoard>
--- sites/superconnectors/src/stores/time.ts
+++ sites/superconnectors/src/stores/time.ts
@@ -1,4 +1,17 @@
-import { readable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
+export const _countdown = () => {
+	const { subscribe, set } = writable(0);
+	const currentTime = Number(new Date());
+	const deadline = Number(new Date(currentTime + 7 * 60 * 1000));
+	setInterval(function () {
+		const now = new Date().getTime();
+		const distance: number = deadline - now;
+		set(distance);
+	});
+	return { subscribe };
+};
+
+export const countdown = _countdown();
 
 export const time = readable(new Date(), function start(set) {
 	const interval = setInterval(() => {
